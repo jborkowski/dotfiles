0eee2ea775d43c13a917904bfc4106d95dfa348c
Jonatan Borkowski <jonatan@thebo.me>
2023-05-19 00:08:23 +0200

    Add config from macOS

---
+++ fetch-dicts.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+location="~/Library/Spelling/"
+lang=(en_US pl_PL es_ES)
+
+for lang in "${langs[@]}"; do
+    country= $(awk -F'[_.]' '{print $1}' <<< "$lang")
+    wget https://cgit.freedesktop.org/libreoffice/dictionaries/plain/${country}/${lang}.dic -O $location
+    wget https://cgit.freedesktop.org/libreoffice/dictionaries/plain/${country}/${lang}.aff -O $location
+done
