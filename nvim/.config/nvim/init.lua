ebfc35e58189389bc36ab4a8de5bfa44e3503a8f
Jonatan Borkowski <jonatan@thebo.me>
2023-05-19 07:34:30 +0000

    nvim defaults

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,21 @@ vim.g.mapleader = " "
 vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
 
+vim.g.terminal_emulator='zsh'
+
+
+vim.g.autoread = true 
+vim.g.autowrite = true 
+vim.g.splitbelow = true
+
+vim.g.nobackup = true 
+vim.g.noswapfile = true 
+
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+
 require('plugins')
 require('cursor_colemak')
 
