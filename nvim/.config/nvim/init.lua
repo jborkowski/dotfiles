8526a14954e4b1967680f8bff77410f98caa0085
Jonatan Borkowski <jonatan@thebo.me>
2023-06-15 10:36:12 +0200

    fix init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,11 +11,11 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
-vim.o.autowrite = true
-vim.o.autoread = true 
-vim.o.incsearch = true 
-vim.o.nobackup = true 
-vim.o.noswapfile = true 
+vim.g.autowrite = true
+vim.g.autoread = true 
+vim.g.incsearch = true 
+vim.g.nobackup = true 
+vim.g.noswapfile = true 
 
 require('plugins')
 require('cursor_colemak')
