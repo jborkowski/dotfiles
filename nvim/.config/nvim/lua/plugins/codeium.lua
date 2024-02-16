57fbfb06032a03f4c9101eef336e4a1773af069f
Jonatan Borkowski <jonatan@thebo.me>
2024-02-16 07:52:20 +0000

    lets tes codeium

---
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,18 @@
+return {
+  'Exafunction/codeium.vim',
+  event = 'BufEnter',
+  init = function()
+    vim.g.codeium_disable_bindings = 1
+  end,
+  keymaps = {
+    { '<leader>tc', '<cmd>CodeiumToggle<cr>', desc = 'Toggle Codeium' },
+  },
+  config = function()
+    vim.keymap.set('i', '<m-tab>', vim.fn['codeium#Accept'], { noremap = true, expr = true, desc = 'Codeium Accept' })
+    vim.keymap.set('i', '<m-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { noremap = true, expr = true, desc = 'Next Codeium Completion' })
+    vim.keymap.set('i', '<m-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { noremap = true, expr = true, desc = 'Prev Codeium Completion' })
+    vim.keymap.set('i', '<m-x>', vim.fn['codeium#Clear'], { noremap = true, expr = true, desc = 'Codeium Clear' })
+    vim.keymap.set('i', '<m-i>', vim.fn['codeium#Complete'], { noremap = true, expr = true, desc = 'Codeium Complete' })
+  end
+}
+ 
