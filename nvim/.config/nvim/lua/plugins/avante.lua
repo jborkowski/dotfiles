03685620775ee1a90403cd75aa94e540a079d5e4
Jonatan Borkowski <jonatan@thebo.me>
2024-11-10 00:46:32 +0100

    use latest

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -7,7 +7,7 @@ return {
     provider = "claude",
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20240620",
+      model = "claude-3-5-sonnet-latest",
       temperature = 0,
       max_tokens = 4096,
     },
