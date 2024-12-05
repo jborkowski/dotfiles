b787780822773d54105f0f4f3c2f5a319f11dd7e
Jonatan Borkowski <jonatan@thebo.me>
2024-12-05 09:27:09 +0100

    update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -48,7 +48,25 @@ return {
       },
       { "<Leader>/",   builtin('current_buffer_fuzzy_find'),  desc = 'Fuzzy file in file' },
 
-      { "<leader>fg",  builtin('live_grep'),                  desc = "Live Grep (ripgrep)" },
+      {
+        "<leader>fg",
+        builtin('live_grep', {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+            '--follow',
+            '-g',
+            '!.git',
+          }
+        }),
+        desc = "Live Grep (ripgrep)"
+      },
       { "<Leader>fm",  builtin('marks'),                      desc = 'Search marks' },
       { "<leader>hm",  builtin('harpoon', 'marks'),           desc = "Harpoon marks" },
 
