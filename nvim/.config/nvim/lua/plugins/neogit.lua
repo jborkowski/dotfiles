204e1fcc30e4137c42e4b96d237dc5ec619b3982
Jonatan Borkowski <jonatan@thebo.me>
2024-12-05 09:33:07 +0100

    Add gitUI cmd

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -3,16 +3,21 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim", 
+    "nvim-telescope/telescope.nvim",
   },
 
   keys = function()
     return {
-      {"<leader>gg", "<cmd>Neogit cwd=%:p:h <cr>", desc = "Neogit" },
-      {"<leader>gc", "<cmd>Neogit commit cwd=%:p:h <cr>", desc = "Neogit commit" },
+      { "<leader>gg", "<cmd>Neogit cwd=%:p:h <cr>",        desc = "Neogit" },
+      { "<leader>gc", "<cmd>Neogit commit cwd=%:p:h <cr>", desc = "Neogit commit" },
     }
   end,
 
-  config = true
+  config = function()
+    vim.api.nvim_create_user_command('GitUI', function()
+      vim.cmd("tabnew")
+      vim.fn.termopen("gitui")
+      vim.cmd("startinsert")
+    end, {})
+  end
 }
-
