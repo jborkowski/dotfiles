4caabe8fedb4e1685389a60cb943738d18e1db60
Jonatan Borkowski <jonatan@thebo.me>
2025-04-16 23:39:52 +0200

    null and trouble

---
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,24 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.zigfmt,
+        null_ls.builtins.formatting.rustfmt,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("Format", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
+    })
+  end,
+}
