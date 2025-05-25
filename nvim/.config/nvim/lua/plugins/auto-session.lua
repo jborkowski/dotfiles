9f172795fcb4bd48b20f5f8b7ddec574beeb417a
Jonatan Borkowski <jonatan@thebo.me>
2025-05-25 22:59:09 +0200

    add auto-session

---
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,13 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Downloads', '/' },
+    root_dir = '~/.local/state/nvim/sessions/',
+
+  }
+}
