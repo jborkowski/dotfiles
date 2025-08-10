5b150cdd22c2822547bfd784ce13a1bd363ed4c0
Jonatan Borkowski <jonatan@thebo.me>
2025-08-10 19:08:02 +0200

    Add script to wake over kvm

---
+++ scripts/.scripts/wokvm-luddite.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+KVMHOST="ludditekvm"
+POWER_PIN="503"
+RESET_PIN="505"
+GPIO_PATH="/sys/class/gpio/gpio${POWER_PI}"
+
+if ssh "root@${KVMHOST}" -q -o ConnectTimeout=1 -o BatchMode=yes <<EOF
+g
+  echo 1 > "${GPIO_PATH}/value"
+  sleep 1
+  echo 0 > "${GPIO_PATH}/value"
+EOF
+then
+  echo "Done"
+else
+  echo "Failed to connect to ${KVMHOST}" >&2
+  exit 1
+fi
