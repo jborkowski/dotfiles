97974708827d5d73905a9b9b3664ec6883b0e430
Jonatan Borkowski <jonatan@thebo.me>
2025-11-08 08:30:31 +0100

    Fix rust-analyzer to use system installation via rustup
    - Remove broken Mason API call (get_install_path)
    - Use system rust-analyzer installed via rustup instead
    - Fixes E5108 error when opening Rust files

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -9,14 +9,8 @@ vim.g.rustaceanvim = function()
     },
     server = {
       cmd = function()
-        local mason_registry = require('mason-registry')
-        if mason_registry.is_installed('rust-analyzer') then
-          local ra = mason_registry.get_package('rust-analyzer')
-          local ra_filename = ra:get_receipt():get().links.bin['rust-analyzer']
-          return { ('%s/%s'):format(ra:get_install_path(), ra_filename or 'rust-analyzer') }
-        else
-          return { 'rust-analyzer' }
-        end
+        -- Use system rust-analyzer installed via rustup
+        return { 'rust-analyzer' }
       end,
       on_attach = function(client, bufnr)
         local common = require('plugins.lspconfig.common')
