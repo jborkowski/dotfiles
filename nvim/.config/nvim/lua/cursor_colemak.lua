1f8bf3189f6e74e0ebecf8b197879c35da0be804
Jonatan Borkowski <jonatan@thebo.me>
2026-01-30 11:21:44 +0100

    Fix window navigation shortcuts conflicting with Neogit git operations
    The s* keybindings for window navigation were colliding with Neogit's
    s (stage), u (unstage), and x (discard) operations. Added filetype
    check to disable these mappings in Neogit buffers, allowing git
    operations to work without interference.

--- nvim/.config/nvim/lua/cursor_colemak.lua
+++ nvim/.config/nvim/lua/cursor_colemak.lua
@@ -6,16 +6,35 @@ utils.set_keymap("H", "5h")
 utils.set_keymap("L", "5l")
 
 
--- moving the cursor around windows
-vim.keymap.set("n", "sw", "<C-w>w", { noremap = true })
-vim.keymap.set("n", "sn", "<C-w>h", { noremap = true })
-vim.keymap.set("n", "se", "<C-w>j", { noremap = true })
-vim.keymap.set("n", "si", "<C-w>k", { noremap = true })
-vim.keymap.set("n", "so", "<C-w>l", { noremap = true })
+-- moving the cursor around windows (skip in git buffers)
+local function not_git_buffer()
+  local ft = vim.bo.filetype
+  return ft ~= "NeogitStatus" and ft ~= "NeogitCommitMessage" and ft ~= "NeogitPopup"
+end
+
+vim.keymap.set("n", "sw", function()
+  if not_git_buffer() then vim.cmd("wincmd w") end
+end, { noremap = true })
+vim.keymap.set("n", "sn", function()
+  if not_git_buffer() then vim.cmd("wincmd h") end
+end, { noremap = true })
+vim.keymap.set("n", "se", function()
+  if not_git_buffer() then vim.cmd("wincmd j") end
+end, { noremap = true })
+vim.keymap.set("n", "si", function()
+  if not_git_buffer() then vim.cmd("wincmd k") end
+end, { noremap = true })
+vim.keymap.set("n", "so", function()
+  if not_git_buffer() then vim.cmd("wincmd l") end
+end, { noremap = true })
 
 -- split the screens to horizontal (down) and vertical (right)
-vim.keymap.set("n", "sh", ":set splitbelow<CR>:split<CR>", { noremap = true })
-vim.keymap.set("n", "sv", ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>", { noremap = true })
+vim.keymap.set("n", "sh", function()
+  if not_git_buffer() then vim.cmd("set splitbelow | split") end
+end, { noremap = true })
+vim.keymap.set("n", "sv", function()
+  if not_git_buffer() then vim.cmd("set nosplitright | vsplit | set splitright") end
+end, { noremap = true })
 
 local map = vim.keymap.set
 
