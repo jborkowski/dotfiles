d45a6c77d191742f633b7cf919ad550d93e8e81f
Jonatan Borkowski <jonatan@thebo.me>
2026-03-05 22:27:23 +0100

    FIX shit

--- nvim/.config/nvim/lua/plugins/purescript.lua
+++ nvim/.config/nvim/lua/plugins/purescript.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "https://forge.id1.in/aj/nvimmer-ps.git", -- backup of srghma/nvimmer-ps
+    "srghma/nvimmer-ps",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
