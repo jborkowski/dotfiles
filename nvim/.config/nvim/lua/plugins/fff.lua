df2f26cc8abba6cb529028fc20b28036603ab339
Jonatan Borkowski <jonatan@Mac.lan>
2026-04-18 21:47:53 +0200

    use prebuild

--- nvim/.config/nvim/lua/plugins/fff.lua
+++ nvim/.config/nvim/lua/plugins/fff.lua
@@ -1,6 +1,10 @@
 return {
   "dmtrKovalenko/fff.nvim",
-  branch = "main",
+  build = function()
+    -- this will download prebuild binary or try to use existing rustup toolchain to build from source
+    -- (if you are using lazy you can use gb for rebuilding a plugin if needed)
+    require("fff.download").download_or_build_binary()
+  end,
   build = "cargo build --release",
   opts = {},
   keys = {
